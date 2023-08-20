ad44af720c49b8bc007788c4cbcd7a68acc2bb24
k-46 <kamalesh.a@kalvium.community>
2023-08-20 15:29:50 +0530

    added the JS part

--- src/app.js
+++ src/app.js
@@ -23,11 +23,18 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    let detailedPlayers =[]
+    detailedPlayers = players.map(function(names,index){
+        return{
+        name: names,
+        strength : getRandomStrength(),
+        image: `images/super-${index+1}.png`,
+        type : index%2==0 ? "hero" : "villain"
+    }});
 
     // Instead of forloop use Map method
     // Code here
-
+console.log(detailedPlayers)
     return detailedPlayers;
 }
 
@@ -39,12 +46,34 @@ const getRandomStrength = () => {
 // Build player template
 const buildPlayers = (players, type) => {
     let fragment = '';
-
+    let hero = players.filter(function(item){
+        return item.type=="hero";
+    })
+    let villain = players.filter(function(item){
+        return item.type=="villain";
+    })
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    if(type=="hero"){
+    fragment=hero.map(function(item){
+        return `<div class="player">
+        <img src="${item.image}" alt="">
+        <div class="name">${item.name}</div>
+        <div class="strength">${item.strength}</div>
+        </div>`
+    })}
+    else if(type=="villain"){
+        fragment=villain.map(function(item){
+            return `<div class="player">
+            <img src="${item.image}" alt="">
+            <div class="name">${item.name}</div>
+            <div class="strength">${item.strength}</div>
+            </div>`
+        })
+    }
 
-    return fragment;
+    return fragment.join('\n');
 }
 
 // Display players in HTML
